d2c37a9dc51ef27a0b5bea1af92aba6c8905e279
ritikasharma78 <bewithritika17@gmail.com>
2026-06-23 16:13:13 +0530

    migrated to resend

--- server/config/nodeMailer.js
+++ server/config/nodeMailer.js
@@ -1,23 +1,12 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async ({ to, subject, body }) => {
-  const response = await transporter.sendMail({
-    from: process.env.SENDER_EMAIL,
+export const sendEmail = async ({ to, subject, body }) => {
+  await resend.emails.send({
+    from: "TicketTap <onboarding@resend.dev>",
     to,
     subject,
     html: body,
   });
-
-  return response;
 };
-
-export { sendEmail };
